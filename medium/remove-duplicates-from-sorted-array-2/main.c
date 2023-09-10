26280ab117198849fce5f2a5e8f22b96f0b5d95a
Timy3005 <76963693+Timy3005@users.noreply.github.com>
2023-09-10 14:25:41 +0200

    first draft

--- medium/remove-duplicates-from-sorted-array-2/main.c
+++ medium/remove-duplicates-from-sorted-array-2/main.c
@@ -1,11 +1,51 @@
 #include <stdio.h>
 
-int removeDuplicates(int *nums, int numsSize)
-{
-  return 0;
+#define COUNT_SIZE 20001
+
+#define COUNT_CONSTANT 10000
+
+int removeDuplicates(int *nums, int numsSize) {
+  int k = 0;
+  int count[COUNT_SIZE] = {0};
+
+  for (int i = 0; i < numsSize; i++) {
+    if (count[nums[i] + COUNT_CONSTANT] == 2) {
+      k++;
+      continue;
+    }
+    count[nums[i] + COUNT_CONSTANT]++;
+  }
+
+  int i, j = 0;
+  while (j < COUNT_SIZE) {
+    if (count[j] == 0) {
+      j++;
+    } else {
+      nums[i] = j - COUNT_CONSTANT;
+      count[j]--;
+      i++;
+    }
+  }
+
+  for (int i = numsSize - 1; i >= numsSize - k; i--)
+  {
+    nums[i] = 0;
+  }
+  return k;
 }
 
-int main()
+void print_array(int *array, int size)
 {
-  return 0;
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d\n", array[i]);
+  }
+}
+
+int main() 
+{
+  int array[] = {1, 1, 1, 2, 2, 3};
+  int k = removeDuplicates(array, 6);
+  print_array(array, 6);
+  printf("\n k = %d\n", k);
 }
